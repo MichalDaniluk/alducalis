0080ce28e72ba424117c3af14cb48254449904c8
MichalDaniluk <forxik@gmail.com>
2022-07-13 23:00:22 +0200

    Added scss, nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Alducalis</title>
+        <meta name="description" content="Alducalis" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Witaj na mojej stronie</h1>
+        </main>
+    </div>
+  )
+}
+
+export default Home
